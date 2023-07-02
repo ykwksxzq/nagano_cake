1025414349a663c98de3a7c2f1654c5ae692c51e
ykwksxzq <132530452+ykwksxzq@users.noreply.github.com>
2023-07-02 02:03:20 +0000

    [add]Order_detailのデータベース作成

---
+++ db/migrate/20230702014240_create_order_details.rb
@@ -0,0 +1,13 @@
+class CreateOrderDetails < ActiveRecord::Migration[6.1]
+  def change
+    create_table :order_details do |t|
+
+      t.integer :order_id, null:false
+      t.integer :item_id, null:false
+      t.integer :purchase_price, null:false
+      t.integer :order_amount, null:false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_02_012511) do
+ActiveRecord::Schema.define(version: 2023_07_02_014240) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -52,6 +52,15 @@ ActiveRecord::Schema.define(version: 2023_07_02_012511) do
     t.string "name", null: false
     t.text "introduction", null: false
     t.integer "price", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "order_details", force: :cascade do |t|
+    t.integer "order_id", null: false
+    t.integer "item_id", null: false
+    t.integer "purchase_price", null: false
+    t.integer "order_amount", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
