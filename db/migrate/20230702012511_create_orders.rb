44b7061bd9c1203e8f3d36fe7fc465dc6b35587f
ykwksxzq <132530452+ykwksxzq@users.noreply.github.com>
2023-07-02 04:29:13 +0000

    [add]CustomerとAdminのデータベース作成

--- db/migrate/20230702012511_create_orders.rb
+++ db/migrate/20230702012511_create_orders.rb
@@ -1,14 +1,14 @@
 class CreateOrders < ActiveRecord::Migration[6.1]
   def change
     create_table :orders do |t|
-      
+
       t.integer :customer_id, null: false
       t.integer :shipping_postal_code, null: false
       t.string :shipping_address, null: false
       t.string :shipping_name, null: false
       t.integer :shipping_cost, null:false
       t.integer :total_payment, null:false
-      t.integer :payment_method, null:false
+      t.integer :payment_method, null:false, default:0
 
       t.timestamps
     end
