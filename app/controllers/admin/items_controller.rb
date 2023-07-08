6e58f207fed9f5988b9942cc5bb963b513a568f0
ykwksxzq <132530452+ykwksxzq@users.noreply.github.com>
2023-07-08 07:07:46 +0000

    [add]管理者 商品編集

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -17,7 +17,16 @@ class Admin::ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
-  
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to admin_item_path(@item.id)
+  end
 
 
 
