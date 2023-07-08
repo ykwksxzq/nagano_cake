b1db2bdee1ba8d86c618e6899bcc019fd8b2e061
ykwksxzq <132530452+ykwksxzq@users.noreply.github.com>
2023-07-08 16:57:51 +0000

    [add]会員編集画面

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -9,6 +9,18 @@ class Admin::CustomersController < ApplicationController
  end
 
 
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admin_customer_path(@customer.id)
+  end
+
+
+
 
 
   private
