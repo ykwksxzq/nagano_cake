517bdfe07693f12b5b7043f6c93d626b1909edaf
ykwksxzq <132530452+ykwksxzq@users.noreply.github.com>
2023-07-10 14:50:40 +0000

    [add]カート画面 削除機能追加

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -12,6 +12,12 @@ class Public::CartItemsController < ApplicationController
     redirect_to cart_items_path
   end
 
+  def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to cart_items_path
+  end
+
 
   private
 
