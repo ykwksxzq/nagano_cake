c71c3a5b7a9cb0e4148c45bd678003f0fdfdb686
ykwksxzq <132530452+ykwksxzq@users.noreply.github.com>
2023-07-16 14:54:52 +0000

    [add]注文情報画面　confim 注文確定

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -22,8 +22,23 @@ class Public::OrdersController < ApplicationController
     @order = Order.new(order_params)
     @order.customer_id = current_customer.id
     @order.save
-    redirect_to orders_confirm_path
+
+    current_customer.cart_items.each do |cart_item|
+      @order_detail = OrderDetail.new
+      @order_detail.item_id = cart_item.item_id
+      @order_detail.purchase_price = cart_item.item.with_tax_price
+      @order_detail.order_amount = cart_item.amount
+      @order_detail.order_id = @order.id
+      @order_detail.save
+    end
+    current_customer.cart_items.destroy_all
+    redirect_to orders_complete_path
   end
+
+  def complete
+  end
+
+
 
   private
 
